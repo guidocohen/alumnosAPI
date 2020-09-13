8ad14a7a34516f37c7129db68b0ac9e1e78a67ac
Guido <guidocohens@gmail.com>
2020-09-13 20:55:44 -0300

    getById

--- apimodel.php
+++ apimodel.php
@@ -27,4 +27,27 @@ class APIModel extends Database
             echo json_encode(array('error' => 'No hay datos que mostrar'));
         }
     }
+
+    function getById($id){
+        $alumnos = array();
+        $alumnos['items'] = array();
+        
+        $query = $this->connect()->prepare("SELECT * FROM alumnos WHERE id= :id");
+        $query->execute(['id' => $id]);
+
+        if($query->rowCount()){
+            while($row = $query->fetch()){
+                $alumno = new Alumno();
+                $alumno->setId($row['id']);
+                $alumno->setNombre($row['nombre']);
+
+                array_push($alumnos['items'], $alumno->toArray('total'));
+            }
+
+            echo json_encode($alumnos);
+        }else{
+            echo json_encode(array('error' => 'No hay datos que mostrar'));
+        }
+    }
+
 }
